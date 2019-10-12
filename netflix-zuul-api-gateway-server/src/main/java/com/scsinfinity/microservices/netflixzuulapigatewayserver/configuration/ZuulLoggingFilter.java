654341a0925f9b833deb9554c72e71c531c616b6
Aditya Narayan <adityanarayan@scsinfinity.com>
2019-10-12 17:10:14 +0530

    added netflix-zuul-api-gateway-server for api gateway

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/scsinfinity/microservices/netflixzuulapigatewayserver/configuration/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.scsinfinity.microservices.netflixzuulapigatewayserver.configuration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private final Logger logger= LoggerFactory.getLogger(ZuulLoggingFilter.class);
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+	HttpServletRequest httpServletRequest= RequestContext.getCurrentContext().getRequest();
+	logger.info("request -> {},request uri -> {}",httpServletRequest,httpServletRequest.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
--- withdrawal-service/src/main/java/com/scsinfinity/microservices/withdrawalservice/serviceClient/AccountServiceProxy.java
+++ withdrawal-service/src/main/java/com/scsinfinity/microservices/withdrawalservice/serviceClient/AccountServiceProxy.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.scsinfinity.microservices.withdrawalservice.dto.AccountInformation;
 
 //@FeignClient(name="account-service",url="localhost:8000")
-@FeignClient(name="account-service")
+//@FeignClient(name="account-service")
+@FeignClient("netflix-zuul-api-gateway-server")
 @RibbonClient(name="account-service")
 public interface AccountServiceProxy {
-	@GetMapping("/accounts/{cardNo}")
+	//@GetMapping("/accounts/{cardNo}")
+	@GetMapping("/account-service/accounts/{cardNo}")
 	public AccountInformation findAccountForCardNo(@PathVariable String cardNo);
 }
